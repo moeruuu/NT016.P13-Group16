54e35f412b858dfe92ec43c71057463fe0246b03
Uyên Thy <uynthyee.313@gmail.com>
2024-11-01 14:30:54 +0700

    Get Connection String

--- API_Server/API_Server/Program.cs
+++ API_Server/API_Server/Program.cs
@@ -1,3 +1,5 @@
+using API_Server.Models;
+using MongoDB.Driver;
 var builder = WebApplication.CreateBuilder(args);
 
 // Add services to the container.
@@ -23,3 +25,17 @@ app.UseAuthorization();
 app.MapControllers();
 
 app.Run();
+
+public class MongoDbContext
+{
+    private readonly IMongoDatabase _db;
+
+    public MongoDbContext(IConfiguration configuration)
+    {
+        var client = new MongoClient(configuration.GetConnectionString("ConnectionString"));
+        _db = client.GetDatabase("");
+    }
+
+    public IMongoCollection<User> Users => _db.GetCollection<User>("User");
+    public IMongoCollection<TokenData> Tokens => _db.GetCollection<TokenData>("Token");
+}
