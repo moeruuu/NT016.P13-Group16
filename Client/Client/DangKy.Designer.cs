4ccc9ba4326a7e726f86cd0970b1dc58bc3790be
dan02045 <ng.xuandan02042005.thptpvt@gmail.com>
2024-11-10 19:45:29 +0700

    add login,signup

---
+++ Client/Client/DangKy.Designer.cs
@@ -0,0 +1,233 @@
+﻿namespace Client
+{
+    partial class DangKy
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            label1 = new Label();
+            label2 = new Label();
+            label3 = new Label();
+            label4 = new Label();
+            label5 = new Label();
+            txtFullname = new TextBox();
+            txtPassword = new TextBox();
+            txtEmail = new TextBox();
+            txtCFPassword = new TextBox();
+            btnSignUp = new Button();
+            btnBack = new Button();
+            btnExit = new Button();
+            txtUsername = new TextBox();
+            label6 = new Label();
+            SuspendLayout();
+            // 
+            // label1
+            // 
+            label1.AutoSize = true;
+            label1.Font = new Font("Cascadia Code", 20F, FontStyle.Bold, GraphicsUnit.Point, 0);
+            label1.ForeColor = Color.FromArgb(0, 64, 64);
+            label1.Location = new Point(191, 9);
+            label1.Name = "label1";
+            label1.Size = new Size(413, 52);
+            label1.TabIndex = 1;
+            label1.Text = "Đăng Ký Tài Khoản";
+            // 
+            // label2
+            // 
+            label2.AutoSize = true;
+            label2.Font = new Font("Cambria", 11F, FontStyle.Bold);
+            label2.ForeColor = Color.FromArgb(0, 64, 64);
+            label2.Location = new Point(47, 463);
+            label2.Name = "label2";
+            label2.Size = new Size(77, 26);
+            label2.TabIndex = 2;
+            label2.Text = "Email:";
+            // 
+            // label3
+            // 
+            label3.AutoSize = true;
+            label3.Font = new Font("Cambria", 11F, FontStyle.Bold);
+            label3.ForeColor = Color.FromArgb(0, 64, 64);
+            label3.Location = new Point(47, 282);
+            label3.Name = "label3";
+            label3.Size = new Size(115, 26);
+            label3.TabIndex = 3;
+            label3.Text = "Mật Khẩu:";
+            // 
+            // label4
+            // 
+            label4.AutoSize = true;
+            label4.Font = new Font("Cambria", 11F, FontStyle.Bold);
+            label4.ForeColor = Color.FromArgb(0, 64, 64);
+            label4.Location = new Point(47, 191);
+            label4.Name = "label4";
+            label4.Size = new Size(164, 26);
+            label4.TabIndex = 4;
+            label4.Text = "Tên Tài Khoản:";
+            // 
+            // label5
+            // 
+            label5.AutoSize = true;
+            label5.Font = new Font("Cambria", 11F, FontStyle.Bold);
+            label5.ForeColor = Color.FromArgb(0, 64, 64);
+            label5.Location = new Point(47, 374);
+            label5.Name = "label5";
+            label5.Size = new Size(214, 26);
+            label5.TabIndex = 5;
+            label5.Text = "Xác Nhận Mật Khẩu:";
+            // 
+            // txtFullname
+            // 
+            txtFullname.Location = new Point(267, 100);
+            txtFullname.Name = "txtFullname";
+            txtFullname.Size = new Size(474, 31);
+            txtFullname.TabIndex = 6;
+            // 
+            // txtPassword
+            // 
+            txtPassword.Location = new Point(267, 277);
+            txtPassword.Name = "txtPassword";
+            txtPassword.PasswordChar = '*';
+            txtPassword.Size = new Size(474, 31);
+            txtPassword.TabIndex = 7;
+            // 
+            // txtEmail
+            // 
+            txtEmail.Location = new Point(267, 458);
+            txtEmail.Name = "txtEmail";
+            txtEmail.Size = new Size(474, 31);
+            txtEmail.TabIndex = 8;
+            // 
+            // txtCFPassword
+            // 
+            txtCFPassword.Location = new Point(267, 369);
+            txtCFPassword.Name = "txtCFPassword";
+            txtCFPassword.PasswordChar = '*';
+            txtCFPassword.Size = new Size(474, 31);
+            txtCFPassword.TabIndex = 9;
+            // 
+            // btnSignUp
+            // 
+            btnSignUp.BackColor = Color.LightCyan;
+            btnSignUp.Font = new Font("Cambria", 11F, FontStyle.Bold, GraphicsUnit.Point, 0);
+            btnSignUp.ForeColor = Color.FromArgb(0, 64, 64);
+            btnSignUp.Location = new Point(267, 530);
+            btnSignUp.Name = "btnSignUp";
+            btnSignUp.Size = new Size(154, 62);
+            btnSignUp.TabIndex = 10;
+            btnSignUp.Text = "Đăng Ký ";
+            btnSignUp.UseVisualStyleBackColor = false;
+            btnSignUp.Click += btnSignUp_Click;
+            // 
+            // btnBack
+            // 
+            btnBack.BackColor = Color.LightCyan;
+            btnBack.Font = new Font("Cambria", 11F, FontStyle.Bold, GraphicsUnit.Point, 0);
+            btnBack.ForeColor = Color.FromArgb(0, 64, 64);
+            btnBack.Location = new Point(522, 530);
+            btnBack.Name = "btnBack";
+            btnBack.Size = new Size(219, 62);
+            btnBack.TabIndex = 11;
+            btnBack.Text = "Quay Lại Trang Đăng Nhập";
+            btnBack.UseVisualStyleBackColor = false;
+            btnBack.Click += btnBack_Click;
+            // 
+            // btnExit
+            // 
+            btnExit.BackColor = Color.LightCyan;
+            btnExit.Font = new Font("Cambria", 11F, FontStyle.Bold, GraphicsUnit.Point, 0);
+            btnExit.ForeColor = Color.FromArgb(0, 64, 64);
+            btnExit.Location = new Point(587, 641);
+            btnExit.Name = "btnExit";
+            btnExit.Size = new Size(154, 62);
+            btnExit.TabIndex = 12;
+            btnExit.Text = "Thoát";
+            btnExit.UseVisualStyleBackColor = false;
+            btnExit.Click += btnExit_Click;
+            // 
+            // txtUsername
+            // 
+            txtUsername.Location = new Point(267, 186);
+            txtUsername.Name = "txtUsername";
+            txtUsername.Size = new Size(474, 31);
+            txtUsername.TabIndex = 14;
+            // 
+            // label6
+            // 
+            label6.AutoSize = true;
+            label6.Font = new Font("Cambria", 11F, FontStyle.Bold);
+            label6.ForeColor = Color.FromArgb(0, 64, 64);
+            label6.Location = new Point(47, 105);
+            label6.Name = "label6";
+            label6.Size = new Size(182, 26);
+            label6.TabIndex = 13;
+            label6.Text = "Tên Người Dùng:";
+            // 
+            // DangKy
+            // 
+            AutoScaleMode = AutoScaleMode.None;
+            BackColor = Color.LightBlue;
+            ClientSize = new Size(814, 754);
+            Controls.Add(txtUsername);
+            Controls.Add(label6);
+            Controls.Add(btnExit);
+            Controls.Add(btnBack);
+            Controls.Add(btnSignUp);
+            Controls.Add(txtCFPassword);
+            Controls.Add(txtEmail);
+            Controls.Add(txtPassword);
+            Controls.Add(txtFullname);
+            Controls.Add(label5);
+            Controls.Add(label4);
+            Controls.Add(label3);
+            Controls.Add(label2);
+            Controls.Add(label1);
+            Font = new Font("Cambria", 10F, FontStyle.Regular, GraphicsUnit.Point, 0);
+            FormBorderStyle = FormBorderStyle.None;
+            Name = "DangKy";
+            StartPosition = FormStartPosition.CenterScreen;
+            Text = "DangKy";
+            ResumeLayout(false);
+            PerformLayout();
+        }
+
+        #endregion
+        private Label label1;
+        private Label label2;
+        private Label label3;
+        private Label label4;
+        private Label label5;
+        private TextBox txtFullname;
+        private TextBox txtPassword;
+        private TextBox txtEmail;
+        private TextBox txtCFPassword;
+        private Button btnSignUp;
+        private Button btnBack;
+        private Button btnExit;
+        private TextBox txtUsername;
+        private Label label6;
+    }
+}
---
+++ Client/Client/DangKy.cs
@@ -0,0 +1,110 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using MongoDB.Driver;
+using MongoDB.Bson;
+using System.Security.Cryptography;
+using Newtonsoft.Json;
+using System.Text.Json;
+using Microsoft.VisualBasic.ApplicationServices;
+using System.Text.RegularExpressions;
+using System.Web;
+
+namespace Client
+{
+    public partial class DangKy : Form
+    {
+        public DangKy()
+        {
+            InitializeComponent();
+        }
+
+
+        private void btnExit_Click(object sender, EventArgs e)
+        {
+            this.Close();
+        }
+
+        private void btnBack_Click(object sender, EventArgs e)
+        {
+            this.Hide();
+            DangNhap dangNhap = new DangNhap();
+            dangNhap.Show();
+        }
+        public bool checkfullname(string fullname)
+        {
+            return Regex.IsMatch(fullname, "^[a-zA-Z]{6,25}$");
+        }
+        public bool checkusername(string username)
+        {
+            return Regex.IsMatch(username, "^[a-zA-Z0-9]{4,15}$");
+        }
+        private bool checkpass(string password)
+        {
+            return Regex.IsMatch(password, @"^[\w]{6,20}$");
+        }
+        private bool checkemail(string email)
+        {
+            return Regex.IsMatch(email, @"^[a-zA-Z0-9_.]{4,20}@gmail.com(.vn|)$");
+        }
+        private async void btnSignUp_Click(object sender, EventArgs e)
+        {
+            string fullname = txtUsername.Text;
+            string username = txtFullname.Text;
+            string password = txtPassword.Text;
+            string cfpassword = txtCFPassword.Text;
+            string email = txtEmail.Text;
+            if (!checkfullname(fullname)) { MessageBox.Show("Vui lòng nhập họ tên người dùng từ 6 đến 20 ký tự, không dùng ký tự đặc biệt."); return; }
+            if (!checkusername(username)) { MessageBox.Show("Vui lòng nhập tên tài khoản từ 4 đến 15 ký tự, không dùng ký tự đặc biệt."); return; }
+            if (!checkpass(password)) { MessageBox.Show("Vui lòng nhập tên tài khoản từ 6 đến 20 ký tự, không dùng ký tự đặc biệt."); return; }
+            if (cfpassword != password) { MessageBox.Show("Mật khẩu không khớp"); return; }
+            if (!checkemail(email)) { MessageBox.Show("Vui lòng nhập đúng định dạng email."); return; }
+
+            //kết nối tới db và kiểm tra xem người dùng đã tồn tại chưa
+            var client = new MongoClient("mongodb+srv://nt106-p13:hocltmkhongvui@clusteruyenthy.wttnb.mongodb.net/");
+            var database = client.GetDatabase("DOAN");
+            var collection = database.GetCollection<BsonDocument>("Users");
+
+            var filter = Builders<BsonDocument>.Filter.Eq("username", username);
+            var existingUser = await collection.Find(filter).FirstOrDefaultAsync();
+
+            if (existingUser != null)
+            {
+                MessageBox.Show("Tên tài khoản đã tồn tại. Vui lòng chọn tên khác.");
+                return;
+            }
+            //Hash password bằng SHA256
+            using (SHA256 al = SHA256.Create())
+            {
+                byte[] inputbyte = Encoding.UTF8.GetBytes(password);
+                byte[] hashbyte = al.ComputeHash(inputbyte);
+                string hashedPassword = BitConverter.ToString(hashbyte).Replace("-", "").ToLower(); 
+
+                var user = new BsonDocument
+        {
+            { "fullname", fullname},
+            { "username", username },
+            { "password", hashedPassword }, // Lưu mật khẩu đã mã hóa
+            { "email", email }
+        };
+
+                // Thêm người dùng vào cơ sở dữ liệu
+                await collection.InsertOneAsync(user);
+
+                MessageBox.Show("Đăng ký thành công!");
+                this.Hide();
+                DangNhap dangNhap = new DangNhap();
+                dangNhap.Show();
+            }
+
+        }
+
+        
+    }
+}
