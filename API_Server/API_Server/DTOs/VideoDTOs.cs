058c6a958b436a3f4afde97964ed5d781a0b6266
Uyên Thy <uynthyee.313@gmail.com>
2024-11-20 17:26:39 +0700

    Add Tag for Videos

--- API_Server/API_Server/DTOs/VideoDTOs.cs
+++ API_Server/API_Server/DTOs/VideoDTOs.cs
@@ -8,7 +8,7 @@ namespace API_Server.DTOs
         [Required(ErrorMessage = "Vui lòng nhập tên phim")]
         [StringLength(100, ErrorMessage = "Tên phim không thể chứa quá 100 kí tự.")]
         public string Title { get; set; }
-        [StringLength(250, ErrorMessage = "Giới thiệu không thể chứa quá 250 kí tự.")]
+        [StringLength(1000, ErrorMessage = "Giới thiệu không thể chứa quá 250 kí tự.")]
         public string Description { get; set; }
         [Required(ErrorMessage = "Vui lòng chọn ảnh xem trước cho video!")]
         //[Url(ErrorMessage = "Đường dẫn ảnh không hợp lệ.")]
