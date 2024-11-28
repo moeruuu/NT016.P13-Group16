44dba6454219f24e2cbedeceb438e42f8bfbc8db
dan02045 <ng.xuandan02042005.thptpvt@gmail.com>
2024-11-28 15:44:37 +0700

    update

--- UITFLIX/UITFLIX/Program.cs
+++ UITFLIX/UITFLIX/Program.cs
@@ -11,7 +11,7 @@ namespace UITFLIX
             // To customize application configuration such as set high DPI settings or default font,
             // see https://aka.ms/applicationconfiguration.
             ApplicationConfiguration.Initialize();
-            Application.Run(new Admin());
+            Application.Run(new LogIn());
            // Application.Run(new PVideo());
         }
     }
