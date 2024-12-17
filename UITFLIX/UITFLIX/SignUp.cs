2923fb92cc76a62191b4950ba5db1f58b009bf44
Uyên Thy <uynthyee.313@gmail.com>
2024-12-17 17:41:14 +0700

    fix email

--- UITFLIX/UITFLIX/SignUp.cs
+++ UITFLIX/UITFLIX/SignUp.cs
@@ -148,7 +148,7 @@ namespace UITFLIX
         }
         public bool checkemail(string email)
         {
-            return Regex.IsMatch(email, @"^[\w]{4,30}@[a-zA-Z0-9.-]+\.[a-zA-Z]{2,}");
+            return Regex.IsMatch(email, @"^[a-zA-Z0-9._%+-]{1,64}@[a-zA-Z0-9.-]+\.[a-zA-Z]{2,}");
         }
         public bool checkpassword(string password)
         {
