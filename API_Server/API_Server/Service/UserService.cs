a36d8c3ede3ed5633831d501ce4bc949761fbb33
khsnhelovorld <23520718@ms.uit.edu.vn>
2024-12-22 12:17:33 +0700

    fix forgetpassword otp

--- API_Server/API_Server/Service/UserService.cs
+++ API_Server/API_Server/Service/UserService.cs
@@ -24,10 +24,6 @@ namespace API_Server.Service
         private readonly EmailService emailService;
         private readonly VideoService videoService;
 
-        //Dùng static để biến không thay đổi
-        private static string currentEmail;
-        private static string currentOTP;
-
         //Lưu dữ liệu tạm thời
         private static Dictionary<string, TempUserData> otpStorage = new Dictionary<string, TempUserData>();
         private readonly ImgurService imgurService;
@@ -156,9 +152,9 @@ namespace API_Server.Service
 
             string hashNewPwd = HashPassword(newpassword);
             if (hashNewPwd == existUser.Password)
-                throw new Exception("Cannot change to the old password.");
-            var update = Builders<User>.Update.Set(u => u.Password, hashNewPwd);
+                throw new Exception("The new password is the same as the old password!");
 
+            var update = Builders<User>.Update.Set(u => u.Password, hashNewPwd);
             var result = await users.UpdateOneAsync(filter, update);
 
             return result.ModifiedCount > 0;
@@ -178,14 +174,22 @@ namespace API_Server.Service
 
             if (forgetPassDTOs.statusCode == 0)
             {
-                //gửi OTP đến email để confirm
-                currentEmail = existUser.Email;
-                currentOTP = new Random().Next(0, 100000).ToString("D6"); ;
+                var otp = new Random().Next(0, 100000).ToString("D6");
+                var tempData = new TempUserData
+                {
+                    User = existUser,
+                    OTP = otp,
+                    Expiration = DateTime.UtcNow.AddMinutes(5)
+                };
 
-                await SendOTPMail(currentEmail, currentOTP, 1);
+                // Lưu vào otpStorage
+                otpStorage[forgetPassDTOs.Email] = tempData;
+
+                await SendOTPMail(forgetPassDTOs.Email, otp, 1);
                 return "OTP sent";
             }
 
+            otpStorage.Remove(forgetPassDTOs.Email);
             return $"k@1 n@y l@ key $iêµ 7uyệ7 mậ7 dµng để 7ạ0 mộ7 k@1 p@$w0rd mớ1 m@ kH0ng cầN p@$w0rd cũ";
         }
 
